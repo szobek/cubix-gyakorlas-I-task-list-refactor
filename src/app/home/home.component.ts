094e54f2a8035ec9076f064ea7eb79ae558eb5c3
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-02 14:44:43 +0200

    base nav

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
+
+@Component({
+  selector: 'cgyir-home',
+  imports: [NavComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'cgyir-nav',
+  imports: [RouterLink,RouterLinkActive],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+
+}
