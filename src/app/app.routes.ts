613f293ef5461dc5cb20ee2cc6fc0cc761dfde55
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-02 18:46:34 +0200

    Add task routing and update navigation for task list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent}
+    { path: 'login', component: LoginComponent},
+    {path: 'tasks', loadChildren: () => import('./task/task.routes').then(m => m.routes) },
 ];
---
+++ src/app/task/task.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+export const routes: Routes = [
+    { path: '', component: ListComponent}
+];
