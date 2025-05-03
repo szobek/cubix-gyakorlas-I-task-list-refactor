1a1edd30611d106753f8f326644a1a92b2d26652
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-03 20:15:19 +0200

    Refactor task list component to display incompleted and completed tasks with appropriate messaging

--- src/app/task/components/list/list.component.ts
+++ src/app/task/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'cgyir-list',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-
+  taskService:TaskService=inject(TaskService)
+completedTasks=computed(()=>this.taskService.tasks().filter(task=>task.completed))
+incompletedTasks=computed(()=>this.taskService.tasks().filter(task=>!task.completed))
 }
