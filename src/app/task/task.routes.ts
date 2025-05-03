f254183573c5b361b6e3ae31be8d34dad10c8be4
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-03 20:06:38 +0200

    Update navigation links and routing for task list and creation

--- src/app/task/task.routes.ts
+++ src/app/task/task.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 export const routes: Routes = [
-    { path: '', component: ListComponent},
+    { path: '', redirectTo: 'list', pathMatch: 'full' },
     { path: 'create', component: CreateComponent},
+    { path: 'list', component: ListComponent},
 
 ];
