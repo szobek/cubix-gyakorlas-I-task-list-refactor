d8ab7d853b92fc5949771d5ebb400cce81db872c
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-06 17:18:55 +0200

    Refactor navigation component; enhance layout with a responsive navbar, add logout functionality, and improve styling for better user experience.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,13 +1,32 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 import { AuthService } from '../auth/services/auth.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'cgyir-nav',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrl: './nav.component.scss',
 })
 export class NavComponent {
-  authService=inject(AuthService);
+  private readonly router = inject(Router);
+  menuOpen: boolean = false;
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
+        this.toggleMenu();
+      });
+  }
+  authService = inject(AuthService);
 }
